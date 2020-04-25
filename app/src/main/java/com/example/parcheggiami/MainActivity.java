65203198b062c3ba242273ddc7a47fb8764cfc3f
rvalentini1 <r.valentini88@gmail.com>
2020-04-25 12:34:37 +0200

    1.0

--- app/src/main/java/com/example/parcheggiami/MainActivity.java
+++ app/src/main/java/com/example/parcheggiami/MainActivity.java
@@ -1,28 +1,102 @@
 package com.example.parcheggiami;
 
+
+
+import android.content.Context;
+import android.content.Intent;
+
 import android.os.Bundle;
-
-import com.google.android.gms.maps.MapView;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.Map;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    private static final String ARG_NAME = "username";
 
-public class MainActivity extends AppCompatActivity {
 
 
+    public static void startActivity(Context context, String username) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(ARG_NAME, username);
+        context.startActivity(intent);
+    }
+
+    FirebaseAuth firebaseAuth;
+    GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        TextView textView = findViewById(R.id.textViewWelcome);
+        if (getIntent().hasExtra(ARG_NAME)) {
+            textView.setText(String.format("Welcome - %s", getIntent().getStringExtra(ARG_NAME)));
+        }
+        findViewById(R.id.buttonLogout).setOnClickListener(this);
+        findViewById(R.id.buttonDisconnect).setOnClickListener(this);
+        findViewById(R.id.buttonMap).setOnClickListener(this);
+        googleSignInClient = GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_SIGN_IN);
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonLogout:
+                signOut();
+                break;
+            case R.id.buttonDisconnect:
+
+                 revokeAccess();
+                break;
+            case R.id.buttonMap:
+                Intent myIntent = new Intent(MainActivity.this, MapsActivity.class);
+
+                MainActivity.this.startActivity(myIntent);
+
+                break;
+        }
+    }
+
+    private void signOut() {
+        // Firebase sign out
+        firebaseAuth.signOut();
+
+        // Google sign out
+        googleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Google Sign In failed, update UI appropriately
+                        Log.w(TAG, "Signed out of google");
+                        Intent myIntent = new Intent(MainActivity.this, Login.class);
+
+                        MainActivity.this.startActivity(myIntent);
+                    }
+                });
+    }
+
+    private void revokeAccess() {
+        // Firebase sign out
+        firebaseAuth.signOut();
+
+        // Google revoke access
+        googleSignInClient.revokeAccess().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Google Sign In failed, update UI appropriately
+                        Log.w(TAG, "Revoked Access");
+                    }
+                });
+    }
 }
--- app/src/main/java/com/example/parcheggiami/MapsActivity.java
+++ app/src/main/java/com/example/parcheggiami/MapsActivity.java
@@ -1,30 +1,97 @@
 package com.example.parcheggiami;
 
-import androidx.fragment.app.FragmentActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    /****private GoogleMap mMap;*/
 
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int REQUEST_CODE = 101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Toast.makeText(MapsActivity.this, "bbb", Toast.LENGTH_LONG).show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+      /*  SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+       // mapFragment.getMapAsync(this);*/
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fetchLocation();
     }
+    private void fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+
+                    currentLocation = location;
+                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude() + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    assert supportMapFragment != null;
+                    supportMapFragment.getMapAsync(MapsActivity.this);
+                }
+            }
+        });
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Sei qui!");
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+        googleMap.addMarker(markerOptions);
+
+        LatLng test = new LatLng(41.089924,14.2539113);
+        markerOptions = new MarkerOptions().position(test).title("parcheggioooo").icon(BitmapDescriptorFactory.fromResource(R.drawable.location));
+
+
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(test));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(test, 5));
+        googleMap.addMarker(markerOptions);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLocation();
+                }
+                break;
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -35,13 +102,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
+    /*@Override
+    public void onMapReady_(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
+    }*/
 }
